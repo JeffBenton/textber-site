3c1ce9b57d01592b62fca541e65b62a02c317b90
Jeff Benton <jeff@frogrock.net>
2015-06-12 03:56:43 -0700

    removed excess

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
-var session = require('express-session');
-var cookieParser = require("cookie-parser");
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './client')));
-app.use(cookieParser());
-app.use(session({secret: 'loltextber123454321'}));
 
 require('./config/mongoose.js');
 require('./config/routes.js')(app);
